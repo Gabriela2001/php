dcb13ae19d0901771743d7f5ba6346029c2858a8
Gabriela2001 <rokagabriela@gmail.com>
2018-05-19 14:25:58 -0600

    hola

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>php</title>
+	</head>
+	<body>
+		<p>
+		<?php
+			//DECLARACION DE VARIALBES
+			$Pcantidad = 17;
+			$Scantidad = 3;
+			$Total = $Pcantidad + $Scantidad;
+			if ($Total >= 18){
+				echo "Mayor a 18";
+			}else{
+				echo "Mayor a 18";
+			}
+	
+
+		?>
+		</p>
+		
+	</body>
+</html>
+
+
